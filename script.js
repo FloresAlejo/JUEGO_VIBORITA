7d0131d268033d34327005d8c313f4fa07d3c616
Flores, Alejo Mohamed <floresalejo3@gmail.com>
2022-05-14 20:12:03 -0300

    update
    -

---
+++ script.js
@@ -0,0 +1,52 @@
+// HTML Elements
+const board = document.getElementById('board');
+const scoreBoard = document.getElementById('scoreBoard');
+const startButton = document.getElementById('start');
+const gameOverSign = document.getElementById('gameOver');
+
+// Game settings
+const boardSize = 10;
+const gameSpeed = 100;
+const squareTypes = {
+    emptySquare: 0,
+    snakeSquare: 1,
+    foodSquare: 2
+};
+const directions = {
+    ArrowUp: -10,
+    ArrowDown: 10,
+    ArrowRight: 1,
+    ArrowLeft: -1,
+};
+
+// Game variables
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const createBoard = () =>{
+    boardSquares.forEach((row, rowIndex) => {
+        row.forEach ( (column, columnindex) => {
+            const squareValue = `${rowIndex}${columnindex}`;
+    });
+}
+
+const setGame = () => {
+    snake = ['00','01','02','03'];
+    score = snake.length;
+    direction = 'ArrowRight';
+    boardSquares = Array.from(Array(boardSize), () => new Array(boardSize).fill(squareTypes.emptySquare));
+    console.log(boardSquares);
+    board.innerHTML = '';
+    emptySquares = [];
+    createBoard();
+}
+
+const startGame = () => {
+    setGame()
+}
+
+startButton.addEventListener('click', startGame);
